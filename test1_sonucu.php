8798d59b689d778973c483703548d5d8edbee86a
FurkanAkinci0 <148947827+FurkanAkinci0@users.noreply.github.com>
2024-10-29 12:54:03 +0300

    Update test1_sonucu.php

--- test1_sonucu.php
+++ test1_sonucu.php
@@ -1,17 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Test Sonucu</title>
-</head>
-<body>
-
-<h2>Test Sonuçları</h2>
-
 <?php
-// Doğru cevaplar (anahtar ve doğru cevaplar burada saklanır)
+// Doğru cevaplar
 $correct_answers = [
     "html" => "HyperText Markup Language",
-    "html_tags" => ["p", "img", "link"], // Çoklu cevap olan bir checkbox
+    "html_tags" => ["p", "img", "link"],
     "programming_languages" => ["HTML", "CSS", "Python", "SQL"],
     "form_tag" => "form",
     "css" => "Cascading Style Sheets"
@@ -21,25 +12,29 @@ $score = 0;
 $total_questions = count($correct_answers);
 
 // Kullanıcının verdiği cevapları kontrol et
-foreach ($correct_answers as $question => $answer) {
-    if (isset($_POST[$question])) {
-        if (is_array($answer)) { // Checkbox veya çoklu seçenek için
-            $user_answers = $_POST[$question];
-            sort($user_answers); // Kullanıcı cevabını sıralama
-            sort($answer); // Doğru cevabı sıralama
-            if ($user_answers === $answer) {
-                $score++;
-            }
-        } else {
-            if ($_POST[$question] == $answer) {
-                $score++;
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    foreach ($correct_answers as $question => $answer) {
+        if (isset($_POST[$question])) {
+            if (is_array($answer)) {
+                $user_answers = $_POST[$question];
+                sort($user_answers);
+                sort($answer);
+                if ($user_answers === $answer) {
+                    $score++;
+                }
+            } else {
+                if ($_POST[$question] == $answer) {
+                    $score++;
+                }
             }
         }
     }
+
+    // Sonuçları göster
+    echo "<p>Toplam Doğru Cevap Sayısı: $score / $total_questions</p>";
 }
 
-// Sonuçları göster
-echo "<p>Toplam Doğru Cevap Sayısı: $score / $total_questions</p>";
+?>
 
 </body>
 </html>
