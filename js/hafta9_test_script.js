c53e5b177a9961e1ac7498419f96927d8916cb68
FurkanAkinci0 <148947827+FurkanAkinci0@users.noreply.github.com>
2024-11-29 09:22:43 +0300

    Update hafta9_test_script.js

--- js/hafta9_test_script.js
+++ js/hafta9_test_script.js
@@ -30,7 +30,7 @@ fetch('../data/hafta9_testData.json')
         // Sonuçları kontrol et ve doğru/yanlış renkleri ekle
         document.getElementById('show-results-btn').addEventListener('click', function () {
             const answers = document.querySelectorAll('input[type="radio"]:checked');
-            let score = 0;
+            let Puan = 0;
 
             answers.forEach((answer, index) => {
                 const question = document.querySelectorAll('.question')[index];
@@ -39,13 +39,13 @@ fetch('../data/hafta9_testData.json')
 
                 if (selectedAnswer === correctAnswer) {
                     question.classList.add('correct-answer');
-                    score++;
+                    Puan++;
                 } else {
                     question.classList.add('incorrect-answer');
                 }
             });
 
-            document.getElementById('result').textContent = `Score: ${score}/${testData.questions.length}`;
+            document.getElementById('result').textContent = `Score: ${Puan}/${testData.questions.length}`;
         });
     })
     .catch(error => console.error('JSON veri yüklenirken bir hata oluştu:', error));
