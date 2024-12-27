739562e10c6c2c1171afc92aea59532a48fc06f6
Furkan <148947827+FurkanAkinci0@users.noreply.github.com>
2024-12-27 09:27:58 +0300

    Add files via upload

---
+++ mysql/data/test/kisi.sql
@@ -0,0 +1,78 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Anamakine: 127.0.0.1
+-- Üretim Zamanı: 27 Ara 2024, 07:27:34
+-- Sunucu sürümü: 10.4.32-MariaDB
+-- PHP Sürümü: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Veritabanı: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `anket`
+--
+
+CREATE TABLE `anket` (
+  `id` int(11) NOT NULL,
+  `cevap` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Tablo döküm verisi `anket`
+--
+
+INSERT INTO `anket` (`id`, `cevap`) VALUES
+(1, 'Evet'),
+(2, 'Hay?r'),
+(3, 'Belki'),
+(4, 'Hay?r'),
+(5, 'Hay?r'),
+(6, 'Hay?r');
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `kisi`
+--
+-- test.kisi tablosu için yapı okuma hatası: #1932 - Table &#039;test.kisi&#039; doesn&#039;t exist in engine
+-- test.kisi tablosu için veri okuma hatası: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `test`.`kisi`&#039; at line 1
+
+--
+-- Dökümü yapılmış tablolar için indeksler
+--
+
+--
+-- Tablo için indeksler `anket`
+--
+ALTER TABLE `anket`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Dökümü yapılmış tablolar için AUTO_INCREMENT değeri
+--
+
+--
+-- Tablo için AUTO_INCREMENT değeri `anket`
+--
+ALTER TABLE `anket`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
